af266bed9b6eb8ad88b2ca66725cecf18f7714dd
elenamilner15 <elenamilner15@gmail.com>
2023-07-30 15:15:51 +0300

    Week 4/Day 1/DailyChallenge.sql added

---
+++ Week 4/Day 1/DailyChallenge.sql
@@ -0,0 +1,27 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- 	create table actors(
+-- 	id serial primary key,
+-- 	first_name varchar(50) NOT NULL,
+-- 	last_name varchar(100) NOT NULL,	
+-- 	age date not NULL,
+-- 	number_oscars smallint NOT NULL
+-- 	);
+	
+-- 	insert into actors(first_name, last_name, age,number_oscars)
+-- 	values ('Matt','Damon','08/10/1970', 5),
+-- 	('George','Clooney','06/05/1961', 2);
+	
+-- 	insert into actors(first_name, last_name, age,number_oscars)
+-- 	values ('Angelina', 'Jolie', '04/06/1975', 1),
+-- 	('Jennifer', 'Aniston', '11/02/1969', 0);
+
+
+--  	select * from actors;
+-- 	select count (*) as total_actors from actors;
+
+-- 	insert into actors(first_name, last_name, age, number_oscars)
+-- 	values ('Meryl','', '22/06/1949', 3)
+ 	select * from actors;
+	
+
+	
