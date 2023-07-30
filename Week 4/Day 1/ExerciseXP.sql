d4423d67496a38a0bd6e7d5356f4608d92b46a0f
elenamilner15 <elenamilner15@gmail.com>
2023-07-30 12:27:09 +0300

    XP added

---
+++ Week 4/Day 1/ExerciseXP.sql
@@ -0,0 +1,35 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- 	create table items(
+-- 	id serial primary key,
+-- 	name varchar(50),
+-- 	price integer
+-- 	);
+	
+-- 	insert into items(name, price)
+--  	values('Small Desk', '100'),
+-- 	('Large desk', '300'),
+-- 	('fan', '80');
+	
+--  	create table customers(
+--  	id serial primary key,
+--  	first_name varchar(50),
+-- 	last_name varchar(50)
+-- 	);
+
+-- 	insert into customers(first_name, last_name)
+-- 	values('Greg', 'Jones'),
+-- 	('Sandra', 'Jones'),
+-- 	('Scott', 'Scott'),
+-- 	('Trevor', 'Green'),
+-- 	('Melanie', 'Johnson');
+
+
+-- 	select * from items;
+-- 	select * from items where price >80;
+-- 	select * from items where price <=300;
+
+-- 	select * from customers where last_name = 'Smith';
+-- 	select * from customers where last_name = 'Jones';
+-- 	select * from customers where last_name != 'Scott';
+		   
+	
